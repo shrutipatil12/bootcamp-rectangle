5d20f2dba386ecce24f85c106b0deb4ccc7c65b2
shrutidhananjaypatil <shrutidpatil12@gmail.com>
2019-09-05 01:04:00 +0530

    shruti add readme file

--- src/test/java/com/thoughtworks/bootcamp/RectangleTest.java
+++ src/test/java/com/thoughtworks/bootcamp/RectangleTest.java
@@ -1,9 +1,14 @@
 package com.thoughtworks.bootcamp;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RectangleTest {
+
+//    @BeforeEach(){
+//
+//    }
     @Test
     public void givenZeroLengthAndBreadth_whenCalculateArea_ThenshouldReturnZero(){
             float length = 0.0f;
@@ -18,6 +23,13 @@ public class RectangleTest {
     }
     @Test
     public void givenLengthAndBreadthTwo_whenCalculateArea_ThenshouldReturnFour(){
+        float length = 2.0f;
+        Rectangle rect = new Rectangle(length);
+        Assertions.assertEquals(4.0f,rect.CalculateArea());
+    }
+
+    @Test
+    public void givenLengthFiveAndBreadthEight_whenCalculateArea_ThenshouldReturnFourty(){
         float length = 2.0f;
         Rectangle rect = new Rectangle(length);
         Assertions.assertEquals(4.0f,rect.CalculateArea());
